be4bad909d38ee78c2bcebf90936905d6a871759
Teddy230998 <teddyseptian000@gmail.com>
2019-12-12 11:31:30 +0700

    java-pert-13

---
+++ 07 memahami string/src/pkg07/memahami/string/MemahamiString.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg07.memahami.string;
+
+/**
+ *
+ * @author baciel
+ */
+public class MemahamiString {
+
+    public static void main(String[] args){
+        System.out.println("hallo " + args[0] + " ganteng");
+        System.out.println("hallo " + args[1] + " Manise");
+    }
+}
